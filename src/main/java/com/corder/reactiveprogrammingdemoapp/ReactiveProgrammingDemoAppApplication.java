5edcbab03b9e6fe8f153b866ea8d2964f520ee2f
mathanki <mathanki091suren@gmail.com>
2026-03-04 10:58:47 -0500

    first commit

---
+++ src/main/java/com/corder/reactiveprogrammingdemoapp/ReactiveProgrammingDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.corder.reactiveprogrammingdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveProgrammingDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/corder/reactiveprogrammingdemoapp/ReactiveProgrammingDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corder.reactiveprogrammingdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProgrammingDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
